e001051a04576dc7149f978c3c13639008697a37
Will Holland <william.holland@codethink.co.uk>
2016-02-04 13:42:20 +0000

    toggle larry face between commands

--- larry.zsh
+++ larry.zsh
@@ -9,8 +9,19 @@ function grumpy_larry() {
     return 1
 }
 
-# closing bracket character ) is escaped with %
-grumpy_larry="$(echo $grumpy_larry | sed 's/)/%)/')"
-
 # if exit zero then happy_larry else grumpy_larry
 LARRY="%(?."$happy_larry"."$grumpy_larry")"
+
+toggle=false
+function precmd() {
+    if $toggle; then
+        grumpy_larry='( ͡°Д ͡°)'
+        toggle=false
+    else
+        grumpy_larry='( ͡°⏠ ͡°)'
+        toggle=true
+    fi
+    grumpy_larry="$(echo $grumpy_larry | sed 's/)/%)/')"
+    LARRY="%(?."$happy_larry"."$grumpy_larry")"
+    PROMPT="%{$fg_bold[yellow]%}$LARRY %{$reset_color%}%# "
+}
