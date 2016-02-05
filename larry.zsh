84d1feddef4ec3cf389382aaa5c6776fa3481405
Will Holland <william.holland@codethink.co.uk>
2016-02-05 09:41:25 +0000

    make larry get even more angry
    this is not ready for master because my prompt is hardcoded, I need to figure
    out how to reload the prompt from precmd

--- larry.zsh
+++ larry.zsh
@@ -1,12 +1,14 @@
 happy_larry='( ͡° ͜ ͡°)'
+neutral_larry='( ͡°_ ͡°)'
 grumpy_larry='( ͡°⏠ ͡°)'
+very_grumpy_larry='( ͡°Д ͡°)'
 
 function happy_larry() {
-    return 0
+    larry_happiness=$(($larry_happiness+1))
 }
 
 function grumpy_larry() {
-    return 1
+    larry_happiness=$(($larry_happiness-1))
 }
 
 function escape_str() {
@@ -14,10 +16,36 @@ function escape_str() {
     echo "$1" | sed -r 's/[).]/%&/g'
 }
 
-function Larry() {
-    grumpy_larry="$(escape_str $grumpy_larry)"
-    # if exit zero then happy_larry else grumpy_larry
-    LARRY="%(?."$happy_larry"."$grumpy_larry")"
+function init_larry() {
+    larry_happiness=0
 }
 
-Larry
+function precmd() {
+    if [[ "$?" == 0 ]]; then
+        if [[ $larry_happiness -gt 0 ]]; then
+            larry_happiness=$(($larry_happiness+1))
+        else
+            larry_happiness=1
+        fi
+    else
+        if [[ $larry_happiness -le 0 ]]; then
+            larry_happiness=$(($larry_happiness-1))
+        else
+            larry_happiness=0
+        fi
+    fi
+    Larry
+}
+
+function Larry() {
+    if [[ $larry_happiness -gt 0 ]]; then
+        LARRY="$(escape_str $happy_larry)"
+    elif [[ $larry_happiness -lt "-4" ]]; then
+        LARRY="$(escape_str $very_grumpy_larry)"
+    else
+        LARRY="$(escape_str $grumpy_larry)"
+    fi
+    PROMPT="%{$fg_bold[yellow]%}$LARRY %{$reset_color%}%# "
+}
+
+init_larry
