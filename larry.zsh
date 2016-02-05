1d1a61426eb8716d8ca192f59bc9142ae547ce3b
Will Holland <william.holland@codethink.co.uk>
2016-02-05 13:50:25 +0000

    Fix hardcoded prompt

--- larry.zsh
+++ larry.zsh
@@ -1,14 +1,19 @@
+# TODO very_* should go to neutral on state switch
+very_happy_larry='(  ͡ ͜  ͡)'
 happy_larry='( ͡° ͜ ͡°)'
 neutral_larry='( ͡°_ ͡°)'
 grumpy_larry='( ͡°⏠ ͡°)'
 very_grumpy_larry='( ͡°Д ͡°)'
 
+LARRY_MARKER="{{LARRY}}"
+
 function happy_larry() {
-    larry_happiness=$(($larry_happiness+1))
+    larry_happiness=$(($larry_happiness+4))
 }
 
 function grumpy_larry() {
-    larry_happiness=$(($larry_happiness-1))
+    larry_happiness=$(($larry_happiness-5))
+    return 1
 }
 
 function escape_str() {
@@ -16,8 +21,25 @@ function escape_str() {
     echo "$1" | sed -r 's/[).]/%&/g'
 }
 
-function init_larry() {
-    larry_happiness=0
+
+function larry_prompt_interp(){
+    declare -r local to_parse="$1"
+    declare -r local larry="$2"
+    echo "$to_parse" | sed s/"$LARRY_MARKER"/"$larry"/
+}
+
+function Larry() {
+    if [[ $larry_happiness -gt 4 ]]; then
+        LARRY="$(escape_str $very_happy_larry)"
+    elif [[ $larry_happiness -gt 0 ]]; then
+        LARRY="$(escape_str $happy_larry)"
+    elif [[ $larry_happiness -lt "-4" ]]; then
+        LARRY="$(escape_str $very_grumpy_larry)"
+    else
+        LARRY="$(escape_str $grumpy_larry)"
+    fi
+    PROMPT="$(larry_prompt_interp $LARRY_BKUP_PROMPT $LARRY)"
+    RPROMPT="$(larry_prompt_interp $LARRY_BKUP_RPROMPT $LARRY)"
 }
 
 function precmd() {
@@ -37,15 +59,10 @@ function precmd() {
     Larry
 }
 
-function Larry() {
-    if [[ $larry_happiness -gt 0 ]]; then
-        LARRY="$(escape_str $happy_larry)"
-    elif [[ $larry_happiness -lt "-4" ]]; then
-        LARRY="$(escape_str $very_grumpy_larry)"
-    else
-        LARRY="$(escape_str $grumpy_larry)"
-    fi
-    PROMPT="%{$fg_bold[yellow]%}$LARRY %{$reset_color%}%# "
+function init_larry() {
+    larry_happiness=0
+    LARRY_BKUP_PROMPT="$PROMPT"
+    LARRY_BKUP_RPROMPT="$RPROMPT"
 }
 
 init_larry
